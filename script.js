138bc96f91b90bb31a684190bd830d59a9476ea1
Nawadan1215 <nawadan37+github@gmail.com>
2025-06-14 00:28:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function sayHello() {
-    alert("こんにちは！私のページにようこそ！");
+    alert(".o88b.  .d88b.   .d88b.  db
+d8P    Y8       .8PY8. .8P 88
+8P      88        88 88    88 88
+8b      88        88 88    88 88
+Y8b  d8  8b  d8' 8b  d8' 88booo.
+  Y88P'   Y88P'   `Y88P'  Y88888P");
 }
