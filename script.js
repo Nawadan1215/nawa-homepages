8f919f17690dc9f3e71834a323d780c450417598
Nawadan1215 <nawadan37+github@gmail.com>
2025-06-14 00:29:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 function sayHello() {
-    alert(".o88b.  .d88b.   .d88b.  db
-d8P    Y8       .8PY8. .8P 88
-8P      88        88 88    88 88
-8b      88        88 88    88 88
-Y8b  d8  8b  d8' 8b  d8' 88booo.
-  Y88P'   Y88P'   `Y88P'  Y88888P");
+    alert(".o88b.  .d88b.   .d88b.  db\nd8P    Y8       .8PY8. .8P 88\n8P      88        88 88    88 88\n8b      88        88 88    88 88\nY8b  d8  8b  d8' 8b  d8' 88booo.\n  Y88P'   Y88P'   `Y88P'  Y88888P");
 }
